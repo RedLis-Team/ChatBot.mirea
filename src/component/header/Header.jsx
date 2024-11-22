f1a9a2539859e3ae8e131eb6a84dfd7f01296266
vladindov <vladysvidos@gmail.com>
2024-11-22 18:24:08 +0300

    feat(#1): v1

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -6,6 +6,9 @@ export function Header() {
     return (
         <header className='header'>
             <FaBots className='header__icon' />
+            <div className='header__text'>
+                <h1>Институт искусственного интеллекта</h1>
+            </div>
         </header>
     )
 }
